abce68e900a9bf0a3cbe0943296e3be2dc73a103
Nailahbradley <bradleynailah@gmail.com>
2022-02-07 15:34:14 -0600

    Starting over weather app design

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,8 @@
 import "./App.css";
 import React from "react";
 
-import Search from "./Search";
-import Header from "./Header";
-import Date from "./DateAndDescription";
-import Degrees from "./Degrees";
-import HumidityandWind from "./HumidityandWind";
-import Footer from "./Footer";
-
-import "./container.css";
-
 function App() {
-  return (
-    <div className="Weather">
-      <div className="container">
-        <div className="weather-app-wrapper">
-          <div className="weather-app">
-            <Search />
-            <Header />
-            <Date />
-            <Degrees />
-            <HumidityandWind />
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </div>
-  );
+  return <p>Start your compoment and property here</p>;
 }
 
 export default App;
