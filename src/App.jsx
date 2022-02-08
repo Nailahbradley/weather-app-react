10a6751578b8d9230d457c026d37a3bfe1d856d8
Nailahbradley <bradleynailah@gmail.com>
2022-02-08 10:58:27 -0600

    implemented bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import "./App.css";
+
 import React from "react";
 
 export default function Weather() {
   return (
-    <div className="Weather">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Nailah Bradley and is{" "}
-        <a
-          href="https://github.com/Nailahbradley/weather-app-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Open-sourced on GitHub{" "}
-        </a>
-      </footer>
+    <div className="Weather App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Nailah Bradley and is{" "}
+          <a
+            href="https://github.com/Nailahbradley/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Open-sourced on GitHub{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
