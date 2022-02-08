f1d43b0e87b56f167954f4299a122e608de80e5e
Nailahbradley <bradleynailah@gmail.com>
2022-02-08 10:12:00 -0600

    I added my Github url

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,20 @@ import "./App.css";
 import React from "react";
 
 export default function Weather() {
-  return <div className="Weather">Weather</div>;
+  return (
+    <div className="Weather">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Nailah Bradley and is{" "}
+        <a
+          href="https://github.com/Nailahbradley/weather-app-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Open-sourced on GitHub{" "}
+        </a>
+      </footer>
+    </div>
+  );
 }
