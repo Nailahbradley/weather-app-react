ab05dde962239876bc8a383e8c01d28cc8371c97
Nailahbradley <bradleynailah@gmail.com>
2022-02-08 10:02:11 -0600

    Deleted transfer code to create a brand new weather app

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,6 @@
 import "./App.css";
 import React from "react";
 
-import Search from "./Search";
-import Header from "./Header";
-import Date from "./Date";
-import Degree from "./Degree";
-import HumidityandWind from "./HumidityandWind";
-import Footer from "./Footer";
-
-import "./container.css";
-
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <div className="container">
-        <div className="weather-app-wrapper">
-          <div className="weather-app">
-            <Search />
-            <Header />
-            <Date />
-            <Degree />
-            <HumidityandWind />
-          </div>
-          <Footer />
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="Weather">Weather</div>;
 }
