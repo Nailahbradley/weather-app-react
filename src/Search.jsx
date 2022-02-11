82cf68c67fd35f4084d788d200461152543bd7b7
Nailahbradley <bradleynailah@gmail.com>
2022-02-11 08:38:00 -0600

    Added animation and designing in css

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,16 +1,49 @@
 import React from "react";
 import "./search.css";
 import { BsClouds } from "react-icons/bs";
+import { BsSearch } from "react-icons/bs";
 
 export default function Search() {
   return (
     <div className="Weather">
-      <div class="Search bar">Search bar and button</div>
+      <div class="Search bar">
+        {" "}
+        <form autoComplete="off">
+          <div className="row">
+            <div className="col-10">
+              {" "}
+              <input
+                type="search"
+                placeholder="Type a city..."
+                className="form-control"
+                autoComplete="off"
+                id="inputCity"
+              />
+            </div>
+            <div className="col-2">
+              <button>
+                {" "}
+                <span className="search-icon">
+                  <BsSearch />
+                </span>{" "}
+              </button>
+            </div>
+          </div>
+        </form>{" "}
+      </div>
+      <br />
       <br />
       <h1 className="city">Chicago</h1>
       <br />
-      <div className="temperature">
-        30 <span className="fahrenheit">°F</span>
+      <div className="row">
+        <div className="col-7">
+          <div className="circle">
+            <div></div>
+          </div>
+          <span className="temperature">
+            30 <span className="fahrenheit">°F</span>
+          </span>
+        </div>
       </div>
       <div className="icon">
         {" "}
